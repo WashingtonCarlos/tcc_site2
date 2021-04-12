f73d1a29abeea642e9f1a46afa46289566effacc
Washington Carlos <wash_28@outlook.com>
2021-04-12 20:21:04 -0300

    Migration

---
+++ database/migrations/2021_04_12_231658_create_usuario_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('cidade');
+            $table->string('email');
+            $table->string('nome');
+            $table->string('endereco');
+            $table->string('cpf');
+            $table->string('nivel_de_acesso');
+            $table->string('estado');
+            $table->string('telefone');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
